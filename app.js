780588401ee5895af6eb2053e6d0a53a1d261d61
dsb123dsb <523598438@qq.com>
2018-01-18 12:24:00 +0800

    用户状态持久化

--- app.js
+++ app.js
@@ -5,6 +5,11 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+// 用户状态持久化三个模块
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const mongoStore = require('connect-mongo')(session);
+
 const path = require('path');  // 设置路径
 const mongoose = require('mongoose');
 const _underscore = require('underscore');
@@ -12,14 +17,23 @@ const Movie = require('./models/movies');
 const User = require('./models/users');
 const port = process.env.PORT || 3000; // 设置端口号，默认3000，process为全局变量，命令PORT=4000 node app.js传入 并启动
 const app = express(); //启动一个服务器
+const dbURL = 'mongodb://localhost/node-mongodb_website';
 
-mongoose.connect('mongodb://localhost/node-mongodb_website');
+mongoose.connect(dbURL);
 
 app.set('views', './views/pages'); // 设置视图默认目录
 
 app.set('view engine', 'pug'); // 设置默认模板引擎pug, 模板文件后缀保持统一
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));//将表单数据 编码解析,_id 是mongodb的默认主键
+app.use(cookieParser()); // cookie中间件，现在需要单独安装cookie-parser,session使用
+app.use(session({ // 设置session
+	secret:'imooc',
+	store: new mongoStore({ // 持久化，重启session也在
+		url:dbURL,
+		collection: 'session'
+	})
+}));
 //body-parser不在和express打包在一起，要单独安装 app.use(express.bodyParser()); // 表单数据格式化
 app.use(express.static(path.join(__dirname, 'public'))); // 静态文件
 app.locals.moment = require('moment'); // locals的本地变量。模板可直接使用
@@ -31,6 +45,8 @@ console.log('website started on port '+ port); // 监听成功打印信息
 
 // index page
 app.get('/', (req, res) => {
+	console.log('user in session');
+	console.log(req.session.user);
 	Movie.fetch((err, movies) => {
 		if(err){
 			console.log(err);
@@ -127,7 +143,8 @@ app.post('/user/signin', (req,res)=>{
 				console.log(err);
 			}
 			if(isMatched){
-				console.log("Password is matched");
+				req.session.user = user; // 写入session,不做处理重启服务就消失了
+				// console.log("Password is matched");
 				return	res.redirect('/');
 			}else{
 				console.log("Password is not matched");
